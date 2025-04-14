7a179200fb9c39eb74bf9533398f8ae65cd0d92c
slaktera <mpakatsakes@gmail.com>
2025-04-14 19:17:44 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,61 +1,213 @@
+-- Game: https://www.roblox.com/games/4581966615
+--[[
+    Bugs: 
+        
 
-local lp = game:service"Players".LocalPlayer;
-local ts = game:service"TweenService";
-local vu = game:service"VirtualUser";
-local rs = game:service"ReplicatedStorage";
-local ws = game:service"Workspace";
-local runs = game:service"RunService";
-local vim = game:service"VirtualInputManager"; 
+    Caution:
+        1. You will get kicked if you rapid fire for long enough. If you want to remove rapid fire then make:
+            local changeVals = {
+                Firemode = "Auto",
+                MaxAmmo = math.huge,
+                Firerate = 2000,
+                Range = 2000000,
+                ReloadTime = 0.1,
+                Accuracy = 0.1,
+                Speed = 250,
+            }
+        into this:
+            local changeVals = {
+                MaxAmmo = math.huge,
+                Range = 2000000,
+                ReloadTime = 0.1,
+                Accuracy = 0.1,
+                Speed = 250,
+            }
 
-local tpBypass = loadstring(game:HttpGet("https://raw.githubusercontent.com/french-spy/roblox-scripts/main/demonfall%20tp%20bypass.lua"))();
+    Unsure:
+        1. Not sure if Firerate and Firemode even work. Removed for now. You can add it back in if you want.
 
-local coords = 
-{
-	["Hayakawa Village"] = CFrame.new(920.434387, 757.866943, -2254.14722, 0.287112683, 0.00420951564, 0.95788759, 0, 0.999990344, -0.00439453963, -0.957896829, 0.00126172812, 0.287109911),
-	["Farm near the waterfall"] = CFrame.new(-1474.21838, 744.153259, -767.348267, 0.999273896, 0.00444891257, -0.0378420725, 0, 0.993160129, 0.116761103, 0.0381026939, -0.116676323, 0.992438853),
-	["Fall's Bridge"] = CFrame.new(-1819.33801, 711.637878, -644.845276, 0.0117059154, -0.136461899, 0.990576148, -1.16415336e-10, 0.990644097, 0.136471242, -0.999931574, -0.0015975209, 0.0115963938),
-	["Okuyia Village"] = CFrame.new(-3304.74292, 703.954041, -1275.20239, 0.00171148917, -0.0544422157, 0.998515487, 0, 0.998517036, 0.0544422902, -0.999998629, -9.31773902e-05, 0.00170895073),
-	["Kamakura Village"] = CFrame.new(-2482.86426, 1161.79785, -1683.6217, -0.0300377496, 0.0205000155, -0.999338567, 0, 0.999789774, 0.0205092691, 0.999548852, 0.000616052304, -0.0300314296),
-	["Slayer's Exam"] = CFrame.new(-5201.61719, 792.619141, -3048.21924, 0.056257762, -1.04497587e-07, 0.998416424, -1.21436461e-08, 1, 1.05347581e-07, -0.998416424, -1.80510398e-08, 0.056257762),
-	["Slayer Corps"] = CFrame.new(-1652.82227, 871.53186, -6450.69141, 0.964885414, 0.0239821076, -0.261574328, 0, 0.995823443, 0.091300793, 0.262671411, -0.0880948007, 0.960855365),
-	["Corps Groove"] = CFrame.new(-3696.42896, 726.615295, -5580.87402, 0.982420027, 0.0160170123, -0.185995907, 0, 0.996312678, 0.0857973248, 0.186684281, -0.0842890069, 0.978797376),
-	["Zenitsu"] = CFrame.new(-2949.40649, 760.085632, -3319.16724, 0.22583659, 0.0343555808, -0.973558843, 2.02746542e-05, 0.999377787, 0.0352713801, 0.974164724, -0.0079853097, 0.225695699)
-};
+    Side Notes:
+        1. Infinite Stamina works weirdly. 3ds pls fix Connection.Function!!!!
+        2. Silent Aim works, the hit marker just doesn't appear
+]]
 
-local function getKeys(t) --Use this for the dropdown table
-	local temp = {};
-	local i = 0;
-	for k,v in pairs(t) do
-		i = i + 1;
-		temp[i] = k;
-	end
-	return temp;
-end
+-- // Valiant ENV
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/ValiantENV.lua"))()
 
-for i,v in pairs(getKeys(coords)) do
-    if coords[v] then
-        local dist = (coords[v].Position - lp.Character.HumanoidRootPart.Position).magnitude;
-        if dist <= 100 then
-        	lp.Character.HumanoidRootPart.CFrame = coords[v];
-        else
-        	tpBypass.bindable:Invoke();
-        	lp.Character.HumanoidRootPart.CFrame = coords[v];
+-- // Vars
+local UIS = game:GetService("UserInputService")
+local RenderStepped = RunService.RenderStepped
+local Heartbeat = RunService.Heartbeat
+local LocalPlayer = Players.LocalPlayer
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded.Wait(LocalPlayer.CharacterAdded)
+local Backpack = LocalPlayer.Backpack
+local Humanoid = Character.WaitForChild(Character, "Humanoid")
+local CurrentCamera = Workspace.CurrentCamera
+local Mouse = LocalPlayer.GetMouse(LocalPlayer)
+local ItemList = ReplicatedStorage.Client.ItemList
+local changeVals = {
+    --Firemode = "Auto",
+    MaxAmmo = math.huge,
+    --Firerate = 2000,
+    Range = 2000000,
+    ReloadTime = 0.1,
+    Accuracy = 0.1,
+    Speed = 250,
+}
+local Remotes = game:GetService("ReplicatedStorage")["_CS.Events"]
+
+-- // Base MT Vars + Funs
+local mt = getrawmetatable(game)
+local backupnamecall = mt.__namecall
+local backupnewindex = mt.__newindex
+local backupindex = mt.__index 
+setreadonly(mt, false)
+
+-- // Get Gun
+function returnGun()
+    for i,v in pairs(Character:GetChildren()) do
+        if v.IsA(v, "Tool") and v.FindFirstChild(v, "MainGunScript") and v.MainGunScript.FindFirstChild(v.MainGunScript, "FireEvent") then
+            return v.Name
         end
     end
 end
+-- // Silent Aim + Anti Kick
+hookfunction(LocalPlayer.Kick, warn)
+hookfunction(LocalPlayer.kick, warn)
 
 
+local ValiantAimHacks = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Experimental%20Silent%20Aim%20Module.lua"))()
+ValiantAimHacks["TeamCheck"] = false
+ValiantAimHacks["HitChance"] = 100
 
---[[Example
-sss:Dropdown("Place Teleport", getKeys(coords), function(t)
-    if coords[t] then
-        local dist = (coords[t].Position - lp.Character.HumanoidRootPart.Position).magnitude;
-		if dist <= 100 then
-			lp.Character.HumanoidRootPart.CFrame = coords[t];
-		else
-			tpBypass.bindable:Invoke();
-			lp.Character.HumanoidRootPart.CFrame = coords[t];
-		end
-	end
+mt.__namecall = newcclosure(function(...)
+    local args = {...}
+    local method = getnamecallmethod()
+    if not checkcaller() then
+        if string.lower(method) == "kick" then
+            return nil
+        end
+    end
+    return backupnamecall(...)
 end)
---]]
+
+local randomBodyPart = {
+    "Head",
+    "UpperTorso",
+    "LowerTorso",
+    "LeftUpperLeg",
+    "RightUpperLeg"
+}
+
+mt.__index = newcclosure(function(t, k)
+    if not checkcaller() and t:IsA("Mouse") and (k == "Hit" or k == "Target") then
+        if ValiantAimHacks.checkSilentAim() then
+            local CPlayer = rawget(ValiantAimHacks, "Selected").Character
+            local randomNum = math.random(1, 5)
+            local targetPart = rawget(randomBodyPart, randomNum) -- // bc of stoopid "anti aimbot" that limits legit players
+            if CPlayer and CPlayer.FindFirstChild(CPlayer, targetPart) then
+                return (k == "Hit" and CPlayer[targetPart].CFrame or CPlayer[targetPart])
+            end
+        end
+    end
+    return backupindex(t, k)
+end)
+print('Done Silent Aim!')
+
+
+--[[ // Trash AC Bypass
+local connections = {
+    Humanoid.GetPropertyChangedSignal(Humanoid, "WalkSpeed"),
+    Workspace.ChildAdded,
+    Character.DescendantAdded
+}
+
+function removeConnections()
+    for _,v in pairs(connections) do
+        for _,connection in pairs(getconnections(v)) do
+            if getfenv(connection.Function).script and getfenv(connection.Function).script.Name == "KeyDetector" then
+                connection:Disable()
+                print('Connection Disabled!')
+            end
+        end
+    end
+end
+removeConnections()
+LocalPlayer.CharacterAdded:Connect(removeConnections)]]
+
+
+-- // Unlimited Stamina + Toggle Silent Aim
+UIS.InputBegan:Connect(function(Key, GPE)
+    if not GPE then
+        if Key.KeyCode == Enum.KeyCode.F4 then 
+            ValiantAimHacks["SilentAimEnabled"] = not ValiantAimHacks["SilentAimEnabled"] 
+            ValiantAimHacks["ShowFOV"] = not ValiantAimHacks["ShowFOV"]
+        end
+        if Key.KeyCode == Enum.KeyCode.LeftShift then Humanoid.WalkSpeed = 23 end
+    end
+end)
+UIS.InputEnded:Connect(function(Key, GPE)
+    if not GPE then
+        if Key.KeyCode == Enum.KeyCode.LeftShift then Humanoid.WalkSpeed = 13 end
+    end
+end)
+
+-- // Gun Mods
+function gunMods()
+    for i,v in pairs(changeVals) do
+        for _,x in pairs(require(ItemList)) do
+            if rawget(x, i) then
+                rawset(x, i, v)
+            end
+        end
+    end
+    print('Done Gun Mods!')
+end
+gunMods()
+
+
+-- // Unlimited Ammo
+function unlimtedAmmo()
+    LocalPlayer.Character.ChildAdded:Connect(function(v)
+        if v.IsA(v, "Tool") and v.FindFirstChild(v, "MainGunScript") then
+            v.Handle.Mag:GetPropertyChangedSignal("Value"):Connect(function()
+                v.Handle.Mag.Value = math.huge
+            end)
+            v.Handle.Mag.Value = math.huge
+            print('Done Unlimited Ammo!')
+        end
+    end)
+end
+unlimtedAmmo()
+
+-- // Unlimited Stamina
+function unlimitedStamina()
+    wait(3)
+    if game:GetService("StarterPlayer").StarterCharacterScripts:FindFirstChild("SprintHandler") then
+        game:GetService("StarterPlayer").StarterCharacterScripts:FindFirstChild("SprintHandler"):Destroy()
+    end
+    for i,v in pairs(getgc()) do
+        if getfenv(v).script and getfenv(v).script.Name == "SprintHandler" then
+            for a,x in pairs(debug.getupvalues(v)) do
+                if type(x) == 'number' then
+                    coroutine.wrap(function()
+                        while wait() do
+                            debug.setupvalue(v, a, 100)
+                        end
+                    end)()
+                end
+            end
+        end
+    end
+    print('Done Unlimited Stamina!')
+end
+unlimitedStamina()
+
+-- // Backpack
+LocalPlayer.PlayerScripts.OwnsBackpackPass.Value = true
+LocalPlayer.CharacterAdded:Connect(function() 
+    LocalPlayer.PlayerScripts:WaitForChild("OwnsBackpackPass").Value = true 
+    print('Got Backpack Gamepass!')
+    unlimtedAmmo()
+    unlimitedStamina()
+end)
